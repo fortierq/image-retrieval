778c690edaef52612af4c79ab930b0b1fbcd2e96
Quentin Fortier <qpfortier@gmail.com>
2021-08-05 21:57:48 +0200

    Try to improve plots

--- utils.py
+++ utils.py
@@ -12,13 +12,18 @@ def rm_dir(path):  # delete a directory
                 rm_dir(child)
         path.rmdir()
 
-def plots(images, caption=None, n=5):
+def plots(images, caption=None, output="image.jpg", n=5):
     import warnings
     warnings.filterwarnings("ignore", message="Glyph.* missing from current font")
 
-    figure = plt.figure(figsize=(5*n, 20))
+    fig = plt.figure(figsize=(5*n, 20))
     for i, f in enumerate(islice(images, n)):
-        figure.add_subplot(1, n, i+1)
-        plt.axis("off")
-        plt.title(caption(i, f))
-        plt.imshow(PIL.Image.open(f))
+        ax = fig.add_subplot(1, n, i+1)
+        ax.axis("off")
+        ax.set_title(caption(i, f))
+        plt.figimage(plt.imread(f))
+    plt.savefig(output, bbox_inches='tight')
+    return plt.imread(output) # fixme
+#     return PIL.Image.open(output) # fixme
+
+#     return fig
